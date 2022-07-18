71875a12f1f85844b773333a26114e054238a178
eyesil62 <yesil.emrah62@gmail.com>
2022-07-18 19:19:08 -0400

    global

---
+++ src/Java/BreakStatementTime.java
@@ -0,0 +1,63 @@
+package Java;
+
+public class BreakStatementTime {
+    public static void main(String[] args) {
+        // break is the where condition meets with requirement. thats where the for loop stops
+//
+//        for ( int i =0; i<=10; i++) {
+//            System.out.println("Hello");
+//
+//            if (i==5){
+//                System.out.println("Word");
+//
+//            }
+//        }
+//
+//
+//        for (int speed = 0; speed<300; speed++ ) {
+//            System.out.println("speed" + speed);
+//
+//            if (speed == 75) { //if jave catches the confition it will stop the loop
+//                break;
+//            }
+//
+
+
+//
+//        }
+//        for (int a= 1; a<9; a+=1) {
+//            System.out.println(a);
+//
+//            if (a % 2 == 0 & a % 3 == 0) {
+//
+//                break;
+//
+//            }
+//        }
+
+
+//        String hello = "hello";
+//        String result = "";
+//        for (int b=0; b<hello.length(); b++) {
+//            if (hello.charAt(b) == 'l') {
+//                break;
+//            }
+//            result = result + hello.charAt(b);
+//        }
+//        System.out.println(result);
+//
+//        // I want to sum numbers from 0 to 50 (included) but if my sum is over 14 i want to stop
+//
+//        int sum = 0;
+//        for (int i = 0; i <= 50; i++) { // i value, 0,1,2,3,4 .. 50
+//           // sum = sum + i;  //0+1+2+3+4+5+ ... +50
+//            //  System.out.println("end result=" +sum);
+//            if ((sum >= 140){
+//            break;
+//            }
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+
+    }
+}
