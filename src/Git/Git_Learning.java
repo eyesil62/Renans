dfba74edcba0877416c21894f1a6cb25bd92486b
eyesil62 <yesil.emrah62@gmail.com>
2022-07-18 20:43:39 -0400

    global

--- src/Git/Git_Learning.java
+++ src/Git/Git_Learning.java
@@ -1,4 +1,42 @@
 package Git;
 
+import javax.sound.midi.Soundbank;
+
 public class Git_Learning {
+    public static void main(String[] args) {
+        System.out.println("Git Notes");
+
+
+
+   /* Git    --- > Git is a Version Control System
+    GitHub --- > It is Storage that keep all your codes versions, It is a cloud storage
+
+
+    git status
+    git add *  --> git add is take file to staging
+    git commit -m"my Msg"  ----> staged all your file.
+    git push -- > it is pushing all files to GitHub
+
+
+
+            .gitignore ---> The file we dont want to share or versioning.
+
+            branching
+
+    Pull Request (PR)
+
+    How do you create PR ? Can you tell me your PR process ?
+
+            1 : I crweate my branch in GitHub
+    2 : I do all my task in there
+    3 : I go to GitHub and Create a PR for Main branch and assign to my manager/co-worker
+    4 : After my manager reviewed he merged and delete my branch
+
+
+    What is diffrence between git fetch and git pull ?
+
+    git pull -- will pull and merge (auto Merging)
+
+    git fetch -- will pull but not merge, We need to explicitly ask to merge( git merge) */
+}
 }
---
+++ src/Java/Cont_Statement.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Cont_Statement {
+    public static void main(String[] args) {
+        // if you run your code and it contains cont it will pass that iteration int for loop
+        // continue is that word we use
+
+
+
+    }
+}
