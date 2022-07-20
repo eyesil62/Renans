109d7f1873b323220f23ef5d3f2825af454ad1d6
eyesil62 <yesil.emrah62@gmail.com>
2022-07-20 18:08:39 -0400

    Merge pull request #2 from eyesil62/Emrah-Branch
    Update branchpractise.java

---
+++ src/StudyHall/Study_Hall.java
@@ -0,0 +1,21 @@
+package StudyHall;
+
+public class Study_Hall {
+
+    /**
+     * String remove duplicates
+     * Write a return that can remove the duplicated values from String
+     *
+     * Ex : RemoveDup ("aaabbbcdddeee")
+     * Ex : Java > jav
+     */
+
+    String str = "abbbbccddeeffff";
+    // I would like to check the "a" here  = string str = str.char(0)
+    // I want to check all the chat here, i need int to find out index number
+    // java index number starts with "0"
+    //
+    String result = ""; // we will store answer in to this variable.
+
+
+}
