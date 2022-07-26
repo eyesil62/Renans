8413d1969f4c7250cbd1c681743cf07b954f329a
eyesil62 <yesil.emrah62@gmail.com>
2022-07-26 00:59:09 -0400

    Merge pull request #2 from eyesil62/Emrah-Branch
    Update branchpractise.java

--- src/Homework/Homework_13.java
+++ src/Homework/Homework_13.java
@@ -7,9 +7,15 @@ public class Homework_13 {
         // how   many   times   "a"   is   found   in   any   given   String:
         //String mystring="java is fun, but sometimes java is just pain.Love java java java";
 
-        String mystring = "java is fun, but sometimes java is just pain. Love java java java";
-        // int c =0;
+        String mystring = "    java   is fun, but sometimes java is just pain. Love java java java";
         int c =mystring.length();
-        System.out.println("a = " + c);
+        for ( int k=0; k<=c; k++) {
+            if (mystring.charAt(k)=='a') {
+                System.out.println();
+                k++;
+            }
+
+        }
+        System.out.println("count of \"a\" is :" );
     }
 }
---
+++ src/Homework/Homework_14.java
@@ -0,0 +1,22 @@
+package Homework;
+
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+public class Homework_14 {
+    public static void main(String[] args) {
+
+        //Write a code with following requirements:
+        //- Define an array with values of {6,7,2,3,90,-2,-90,-122,96,35}
+        //- Print the minimum number in the given array
+
+        int [] num = {6,7,2,3,90,-2,-90,-122,96,35};
+        int minimum = num[3];
+
+        for (int number=-122; number<num.length;number++) {
+            if (minimum < number) {
+                minimum = num[number];
+            }
+        }
+        System.out.println("minimum = " + minimum);
+    }
+}
