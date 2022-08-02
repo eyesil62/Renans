f251cfe191af796424f209ae4ede6281ea70bb03
eyesil62 <yesil.emrah62@gmail.com>
2022-08-01 22:25:53 -0400

    Merge pull request #2 from eyesil62/Emrah-Branch
    Update branchpractise.java

---
+++ src/Homework/Homework_Ziya_15.java
@@ -0,0 +1,34 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Homework_Ziya_15 {
+    public static void main(String[] args) {
+//
+//       /** Write a code where it ask user to enter a Sentence,
+//        Find the longest word in the sentence and print it with the length of it.
+//        ex. ,"I love being outside with my dog but I have a lot to do today"
+//        asnwer: Outside and the length is 7 */
+//
+//       System.out.println("Please enter a sentence");
+//       Scanner scn = new Scanner(System.in);
+//
+//       String sentence = scn.nextLine();
+//
+//       String [] comment = sentence.split(" ");
+//       String max ="";
+//       String longword="";
+//
+//       for (int i = 0; i <comment.length; i++) {
+//           if (comment[i].length()>max){
+//               max= comment[i].length();
+// }}
+
+
+
+
+
+
+
+    }
+}
--- src/Udemy/Topic1_Variable.java
+++ src/Udemy/Topic1_Variable.java
@@ -3,5 +3,15 @@ package Udemy;
 public class Topic1_Variable {
     public static void main(String[] args) {
 
+        int a = 12;
+        a=a+4;
+        System.out.println("a = " + a);
+        a+=3;
+        System.out.println("a = " + a);
+        a/=2.0;
+        System.out.println("a = " +a);
+
+
+
     }
 }
